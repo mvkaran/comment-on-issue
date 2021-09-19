e721c5f7b5dbf5c49031b162deb3c0e35ba20a56
MV Karan <mvkaran@github.com>
2021-09-19 19:15:22 +0530

    Style mods with Rubocop

--- action.rb
+++ action.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'octokit'
 require 'json'
 
@@ -8,38 +10,37 @@ comment = ENV['COMMENT']
 
 issue_comment_payload_file = File.read(ENV['GITHUB_EVENT_PATH'])
 issue_comment_payload = JSON.parse(issue_comment_payload_file)
-issue = issue_comment_payload["issue"]
+issue = issue_comment_payload['issue']
 
 # Initialize Octokit SDK
-gh = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+gh = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
 
 # Check if the Comment was created on an Issue with the desired label and is open
 # If not, exit
 
-unless 
-issue["labels"].any? { |l| l["name"] == label } &&
-issue["state"] == "open"
-    puts "Issue is not open or does not have the required label"
-    exit 0
+unless issue['labels'].any? { |l| l['name'] == label } &&
+       issue['state'] == 'open'
+  puts 'Issue is not open or does not have the required label'
+  exit 0
 end
 
 # If comment already exists, exit
 
-comments = gh.issue_comments(repo, issue["number"])
+comments = gh.issue_comments(repo, issue['number'])
 
 if comments.any? { |c| c[:body] == comment }
-    puts "Comment already exists"
-    exit 0
+  puts 'Comment already exists'
+  exit 0
 end
 
 # Check if all assignees on the Issue have commented. If yes, add comment
 
 commenters = comments.map { |c| c[:user][:login] }.uniq
-assignees = issue["assignees"].map { |a| a["login"] }
+assignees = issue['assignees'].map { |a| a['login'] }
 
 if (assignees - commenters).empty?
-    gh.add_comment(repo, issue["number"], comment)
-    puts "All assignees have commented"
+  gh.add_comment(repo, issue['number'], comment)
+  puts 'All assignees have commented'
 else
-    puts "Not all assignees have commented"
+  puts 'Not all assignees have commented'
 end
